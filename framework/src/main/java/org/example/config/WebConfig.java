54389f3678d1ff1f0c0baf3b120874f66bbfe020
banxforce <3045879549@qq.com>
2024-01-16 17:09:02 +0800

    Update: 1、抽取spring配置纸framework模块的application.yaml； 2、为admin模块添加SwaggerConfig

--- framework/src/main/java/org/example/config/WebConfig.java
+++ framework/src/main/java/org/example/config/WebConfig.java
@@ -4,14 +4,9 @@ import com.alibaba.fastjson2.JSONReader;
 import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.support.config.FastJsonConfig;
 import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageConverter;
-import com.fasterxml.jackson.databind.SerializationConfig;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
@@ -20,41 +15,39 @@ import java.util.List;
 
 /**
  * 拦截器阶段：WebMvcConfigurer 中的某些方法确实是在拦截器阶段生效的，比如 addInterceptors 方法用于注册和配置拦截器。
- *
  * 消息转换器阶段：WebMvcConfigurer 接口中的 configureMessageConverters 方法用于配置消息转换器，这个阶段发生在请求被处理之前。
- *
  * 资源处理器阶段：addResourceHandlers 方法用于配置静态资源的处理器，可以在该阶段配置静态资源的访问路径、存放位置等。
- *
  * 视图解析器阶段：configureViewResolvers 方法允许配置视图解析器，这会影响到视图的解析和渲染。
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     /**
-     * 这个方法是在拦截器阶段生效的，security的过滤链由filter组成。
-     * 如果引入了security，这个配置就失效了，需要在security中再进行配置
+     *虽然MVC和Security都使用了 CorsFilter，但它们是独立的实例，分别用于处理 Spring MVC 和 Spring Security 中的请求。
+     * 在绝大多数情况下，只需要在其中一个地方配置 CORS，然后这个配置就会应用到整个应用程序中。
+     * 如果同时在 Spring MVC 和 Spring Security 中都配置了 CORS，通常以 Spring Security 配置为准，因为它在过滤器链中的位置可能更靠前。
      */
     //解决cors跨域问题，与security的cors配置类似
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-            //允许跨域的路径
-        registry.addMapping("/**")
-                //允许跨域的域名源,配合addMapping() = http://localhost:8080/**
-                // 前台和后台
-                //.allowedOrigins("http://localhost:8080","127.0.0.1")
-                //这个方法相对于allowedOrigins来说更灵活:可以指定路径,使用通配符
-                .allowedOriginPatterns("http://localhost:8080","http://localhost:81")
-                //允许携带cookie
-                .allowCredentials(true)
-                //允许所有请求头
-                .allowedHeaders("*")
-                //允许请求的方法
-                .allowedMethods("GET","POST","DELETE","PUT")
-                //公开的请求头，默认客户端只能访问标准的响应头字段，自定义的访问头字段不会被客户端看到
-                //.exposedHeaders("*")
-                //缓存时间,缓存一些跨域配置,实际的请求(GET,POST...)不受影响
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//            //允许跨域的路径
+//        registry.addMapping("/**")
+//                //允许跨域的域名源,配合addMapping() = http://localhost:8080/**
+//                // 前台和后台
+//                //.allowedOrigins("http://localhost:8080","127.0.0.1")
+//                //这个方法相对于allowedOrigins来说更灵活:可以指定路径,使用通配符
+//                .allowedOriginPatterns("http://localhost:8080","http://localhost:81")
+//                //允许携带cookie
+//                .allowCredentials(true)
+//                //允许所有请求头
+//                .allowedHeaders("*")
+//                //允许请求的方法
+//                .allowedMethods("GET","POST","DELETE","PUT")
+//                //公开的请求头，默认客户端只能访问标准的响应头字段，自定义的访问头字段不会被客户端看到
+//                //.exposedHeaders("*")
+//                //缓存时间,缓存一些跨域配置,实际的请求(GET,POST...)不受影响
+//                .maxAge(3600);
+//    }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
